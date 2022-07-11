9655f7406bf7ad26b05e91a135c48b0632bde5f7
sufiyaneng <sufiyaneng@gmail.com>
2022-07-11 07:20:57 +0530

    props initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Props from './Props';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Props message="Sufiyan" number={10} />
+      <Props message="Mazhar" number={101} />
+
     </div>
   );
 }
---
+++ src/Props.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function  Props(props) {
+      return (
+    <div>
+        <h1>Hello {props.message}</h1>
+        <h1>{props.number}</h1>
+        
+    </div>
+  )
+}
+
+export default Props;
